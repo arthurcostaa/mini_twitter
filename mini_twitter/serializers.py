46b4da4468b265d3033686812d145cec6b13005f
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2024-12-14 23:57:24 -0300

    Adicionando validação no email para evitar emails repetidos

--- mini_twitter/serializers.py
+++ mini_twitter/serializers.py
@@ -1,12 +1,25 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(),
+                message='Este email já está em uso. Tente outro e-mail.'
+            )
+        ]
+    )
+    password = serializers.CharField(
+        write_only=True,
+        style={'input_type': 'password'}
+    )
 
     def create(self, validated_data):
         user = User.objects.create_user(
+            email=validated_data['email'],
             username=validated_data['username'],
             password=validated_data['password'],
         )
