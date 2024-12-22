d90a462d66bcf87795e8948bf36ac74d37be292c
Arthur Emanuel Souza Cassiano da Costa <arthurcosta0ac@gmail.com>
2024-12-21 23:06:19 -0300

    Adicionando endpoint para dar like em um post

--- mini_twitter/models.py
+++ mini_twitter/models.py
@@ -5,12 +5,17 @@ from django.db import models
 class Post(models.Model):
     content = models.TextField(max_length=280, blank=False, null=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='post_likes')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     @property
     def total_comments(self):
         return self.comments.count()
+
+    @property
+    def total_likes(self):
+        return self.likes.count()
 
     def __str__(self):
         return self.content[:30]
